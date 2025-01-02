978336f540c6de443f6f90462e5ba7847c62ac18
ZinnunMalikov <97144011+ZinnunMalikov@users.noreply.github.com>
2025-01-02 14:07:15 -0500

    Delete Interactive Game/home_at2.py

--- Interactive Game/home_at2.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image
-graph_thumb = Image.open(r"C:\Users\2000039241\OneDrive - Fulton County Schools\Desktop\meep3.png").convert('RGBA')
-graph_cover = Image.open(r"C:\Users\2000039241\OneDrive - Fulton County Schools\Desktop\Sprouts-main\Resources\Sprouts Cover.png").convert('RGBA')
-graph_icon = Image.open(r"C:\Users\2000039241\OneDrive - Fulton County Schools\Desktop\Sprouts-main\Resources\Gallery Icon.png").convert('RGBA')
-
-
-result = Image.alpha_composite(graph_thumb, graph_cover)
-result.save(r"C:\Users\2000039241\OneDrive - Fulton County Schools\Desktop\meep4.png")
